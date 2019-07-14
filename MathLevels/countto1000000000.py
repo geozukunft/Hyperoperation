5730ed269500b9cd879a93716c743178a79e8160
Viktor geozukunft <geozukunft@gmail.com>
2019-07-15 01:06:53 +0200

    added timestamps

--- MathLevels/countto1000000000.py
+++ MathLevels/countto1000000000.py
@@ -1,16 +1,24 @@
 import datetime
 import time 
 
-start = datetime.datetime.now()
-startstr = str(start)
-x = 1
-while x < 10:
-    x = x + 1
-    print(x)
-    time.sleep(1)
-end = datetime.datetime.now()
-endstr = str(end)
-print("Started: "+ startstr)
-print("Ended: "+ endstr)
-    
 
+
+x = 0
+endnumber = 1000000000
+
+start = datetime.datetime.now()
+
+while x < endnumber:
+    x = x + 1
+
+end = datetime.datetime.now()
+
+print("No console Output has been performed")
+print("Counted to: "+ str(endnumber))
+print("Started: "+ str(start))
+print("Ended: "+ str(end))
+delta = end - start
+deltasec = delta.total_seconds()
+deltams = deltasec * 1000
+print("Time it took: " + str(deltams)+ "ms")
+print("Time it took: " + str(delta))
